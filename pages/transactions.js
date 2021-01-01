1850ee38939e6fa4871fcb87368f4dcf7d5df283
Shaun Sigera <shanilplusplus@gmail.com>
2021-01-01 09:23:30 -0500

    changed transactions.js

--- pages/transactions.js
+++ pages/transactions.js
@@ -1,9 +1,7 @@
-import Link from 'next/link';
 import db from '../src/firebase/firebase';
 import { connect } from 'react-redux';
 import jwt from 'jsonwebtoken'; 
 import cookie from 'cookie';
-import moment from 'moment';
 import Transactions from '../src/components/Transactions';
 
 
@@ -17,30 +15,22 @@ const TransactionsPage = (props) => {
 }
 
 export const getServerSideProps = async(context) => {
-     // auth.onAuthStateChanged(function(user) {
-  //   if (user) {
-  //     localStorage.setItem('User', JSON.stringify(user.uid));
-  //   } else {
-  //     // No user is signed in.
-  //   }
-  // });
-  let decoded = 'dGZZ2xH3toXlfGU2W2F5iifEkMJ3'
+
+  let decoded = 'dGZZ2xH3toXlfGU2W2F5iifEkMJ3';
   if(context.req.headers.cookie){
     const parsedCookies = cookie.parse(context.req.headers.cookie)
     decoded = jwt.decode(parsedCookies.userId, { header: true })
   }
   
   
-  // console.log(parsedCookies.userId,'ucook')
-  
  
-  let kist = [];
-   const dbdb = await db.ref(`users/${decoded}/transactions`)
+  let listOfTransactions = [];
+   const dbreq = await db.ref(`users/${decoded}/transactions`)
   .once('value')
       .then((snapshot) => snapshot.val())
       .then((val) => {
         Object.keys(val).map((key) => {
-          kist.push({
+          listOfTransactions.push({
             id: key,
             ...val[key]
           })
@@ -56,14 +46,14 @@ export const getServerSideProps = async(context) => {
   
   return {
     props : {
-      transactions: [...kist]
+      transactions: [...listOfTransactions]
     }
   }
 }
 
 
 // export default TransactionsPage;
-const mapStateToProps = (state) => {
+const mapStateToProps = () => {
     return {
        
     };
