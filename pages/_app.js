5edcf6a98375d4df4c5a96602971047cedc2dc02
Shaun Sigera <shanilplusplus@gmail.com>
2021-01-01 18:40:46 -0500

    changes to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import {wrapper} from '../src/store/configureStore';
 import '../styles/globals.css';
-// import "react-datepicker/dist/react-datepicker.css";
 import 'react-dates/lib/css/_datepicker.css';
 
 
