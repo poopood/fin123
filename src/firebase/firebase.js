98d9e304db82743a498090481e9dac6a5b9c279e
Shaun Sigera <shanilplusplus@gmail.com>
2021-01-04 09:19:24 -0500

    firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase';
-import { connectAdvanced } from 'react-redux';
 import "firebase/auth";
 
 const firebaseConfig = {
@@ -21,106 +20,8 @@ if (!firebase.apps.length) {
 const db = firebase.database();
 const auth = firebase.auth();
 
-// db.settings({timestampsInSnapshots: true});
 
-
-
-// db.ref().set({
-//     name: 'Shaun Sigera',
-//     age: 30,
-//     isSingle: true
-// });
-
-// db.ref('age').set(29);
-
-// db.ref('attributes').set({
-//     height: '174cm',
-//     weight: '180lb'
-// })
-
-// db.ref('sdf')
-// .remove()
-// .then(() => {
-//     console.log('date was removed')
-// }).catch((e) => {
-//     console.log('did not remove data', e)
-// })
-
-// db.ref().update({
-//     name: 'Shanil'
-// })
-
-// db.ref()
-// .once('value')
-//     .then((snapshot) => {
-//     const val = snapshot.val().transactions;
-//     console.log(val);
-// })
-//     .catch((e) => {
-//     console.log('error fetching data', e)
-// })
-
-// db.ref().on('value', (snapshot) => {
-//     console.log(snapshot.val()); 
-// })
-
-// setTimeout(() => {
-//     db.ref('age').set(32);
-// }, 3000)
-
-// db.ref().on('value', (snapshot) => {
-//     const val = snapshot.val();
-//     console.log(`${val.name} is ${val.isSingle}`)
-// })
-
-// db.ref('notes').push({
-//     title: 'to do',
-//     body: 'go for a run'
-// })
-
-// db.ref('notes')
-// .once('value')
-// .then((snapshot) => {
-//     const notes = [];
-
-//     snapshot.forEach((child) => {
-//         notes.push({
-//             id : child.key,
-//             ...child.val()
-//         })
-//     })
-//     console.log(notes);
-// })
-
-// db.ref('notes').on('value',(snapshot) => {
-//     const notes = [];
-
-//     snapshot.forEach((child) => {
-//         notes.push({
-//             id : child.key,
-//             ...child.val()
-//         })
-//     })
-//     console.log(notes);
-// })
-
-// db.ref('notes').on('child_removed', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// })
-
-//child_changed
-//
 
 export {auth,firebase, db as default };
 
 
-// const libby = dbdb.then((val) => {
-//     const transactions = [];
-
-//     val.forEach((child) => {
-//         transactions.push({
-//             id: val.key,
-//             ...val.val()
-//         })
-//     })
-// // })
