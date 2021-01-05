10cdd848f3729233d8017911aaea812c11886c64
Shaun Sigera <shanilplusplus@gmail.com>
2021-01-04 22:09:40 -0500

    restored next config js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        db_apiKey: "AIzaSyBWOQx-CEVHDIxQ4J9b9v3R2Qzhcjsxw54",
+        db_authDomain: "finnikky-a405e.firebaseapp.com",
+        db_databaseURL: "https://finnikky-a405e.firebaseio.com",
+        db_projectId: "finnikky-a405e",
+        db_storageBucket: "finnikky-a405e.appspot.com",
+        db_messagingSenderId: "751763113401",
+        db_appId: "1:751763113401:web:0b5a4f537eb89f7615e268",
+        db_measurementId: "G-1RP62DBYJB"
+    },
+  }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWOQx-CEVHDIxQ4J9b9v3R2Qzhcjsxw54",
-    authDomain: "finnikky-a405e.firebaseapp.com",
-    databaseURL: "https://finnikky-a405e.firebaseio.com",
-    projectId: "finnikky-a405e",
-    storageBucket: "finnikky-a405e.appspot.com",
-    messagingSenderId: "751763113401",
-    appId: "1:751763113401:web:0b5a4f537eb89f7615e268",
-    measurementId: "G-1RP62DBYJB"
+    apiKey: process.env.db_apiKey,
+    authDomain: process.env.db_authDomain,
+    databaseURL: process.env.db_databaseURL,
+    projectId: process.env.db_projectId,    
+    storageBucket: process.env.db_storageBucket,
+    messagingSenderId: process.env.db_messagingSenderId,
+    appId: process.env.db_appId,
+    measurementId: process.env.db_measurementId
     
 };
 
